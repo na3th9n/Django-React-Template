b411998560f467dcad5c71f1f042488e22f1b8cf
Xie <nathan.xie@magna.com>
2024-04-15 14:52:16 -0400

    Completed frontend and backend connection

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound() {
+    return <div>
+            <div>Not Found</div>
+            <p>The page you are looking for does not exist</p>
+        </div>
+}
+
+export default NotFound
